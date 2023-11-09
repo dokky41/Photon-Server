6ccaea81d315d5e76b0162e46a99abd2c9bfa192
Dokky <107966608+dokky41@users.noreply.github.com>
2023-11-09 21:46:53 +0900

    (2023.11.9) 플레이 맵 SDK추가

---
+++ Assets/Script/PhotonSetting.cs
@@ -0,0 +1,14 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonSetting : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField email;
+    [SerializeField] InputField userID;
+    [SerializeField] InputField password;
+
+  
+}
