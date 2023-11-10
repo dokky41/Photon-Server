540604bd75f0ca2f79f06689d01c7c40d99cf677
Dokky <107966608+dokky41@users.noreply.github.com>
2023-11-10 21:45:25 +0900

    (2023.11.10) 포톤 로비 접속

--- Assets/Script/PhotonSetting.cs
+++ Assets/Script/PhotonSetting.cs
@@ -1,7 +1,10 @@
 using Photon.Pun;
+using PlayFab;
+using PlayFab.ClientModels;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.UI;
 
 public class PhotonSetting : MonoBehaviourPunCallbacks
@@ -10,5 +13,67 @@ public class PhotonSetting : MonoBehaviourPunCallbacks
     [SerializeField] InputField userID;
     [SerializeField] InputField password;
 
-  
+    public void Awake()
+    {
+        
+    }
+
+    public void Function()
+    {
+        Debug.Log("Function");
+    }
+
+    public void LoginSuccess(LoginResult result)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = "1.0";
+
+        PhotonNetwork.NickName = PlayerPrefs.GetString("Name");
+
+        PhotonNetwork.LoadLevel("Photon Lobby");
+
+    }
+
+    public void Login()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = email.text,
+            Password = password.text,
+
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            LoginSuccess,
+            (PlayFabError error) => NotificationManager.NotificationWindow(error.ToString())
+        );
+
+
+    }
+
+    public void SignUp()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = email.text,
+            Password = password.text,
+            Username = userID.text,
+        };
+
+        PlayerPrefs.SetString("Name" , userID.text);
+
+        PlayFabClientAPI.RegisterPlayFabUser
+        (
+            request,
+            (RegisterPlayFabUserResult result) => NotificationManager.NotificationWindow(result.ToString()),
+            (PlayFabError error) => NotificationManager.NotificationWindow(error.ToString())
+
+        );
+
+    }
+
+
 }
