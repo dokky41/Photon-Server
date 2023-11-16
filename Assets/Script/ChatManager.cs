86836c81117974b64b8ddff2ed9670759031ee5d
Dokky <107966608+dokky41@users.noreply.github.com>
2023-11-16 21:46:42 +0900

    (2023.11.16) 채팅시스템구현

--- Assets/Script/ChatManager.cs
+++ Assets/Script/ChatManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
 
-public class ChatManager : MonoBehaviour
+public class ChatManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InputField input;
+    public Transform chatContent;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            input.ActivateInputField();
+
+            if (input.text.Length == 0) return;
+
+            string chat = PhotonNetwork.NickName + " : " + input.text;
+
+            photonView.RPC(nameof(Chatting), RpcTarget.All, chat);
+        }
+
     }
+
+    [PunRPC]
+    void Chatting(string msg)
+    {
+        // chatPrefab 오브젝트를 하나 만들어서 text값에 저장합니다.
+        GameObject chat = Instantiate(Resources.Load<GameObject>("String"));
+
+        chat.GetComponent<Text>().text = msg;
+
+        // 스크롤 뷰 - content 오브젝트의 자식으로 등록합니다.
+        chat.transform.SetParent(chatContent);
+
+        // 채팅을 입력한 후에도 이어서 입력할 수 있도록 설정하는 함수입니다.
+        input.ActivateInputField();
+
+        // 채팅을 입력한 후에 초기화합니다.
+        input.text = null;
+
+    }
+
+
+
 }
--- Assets/Script/PhotonPlayer.cs
+++ Assets/Script/PhotonPlayer.cs
@@ -1,18 +1,99 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class PhotonPlayer : MonoBehaviour
+public class PhotonPlayer : MonoBehaviourPunCallbacks, IPunObservable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TextMeshProUGUI nickName;
+
+    [SerializeField] float score;
+    [SerializeField] float mouseX;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] float speed = 5.0f;
+
+    [SerializeField] Vector3 direction;
+    [SerializeField] Camera temporaryCamera;
+
+    void Awake()
     {
-        
+        nickName.text = photonView.Owner.NickName;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        // 현재 플레이어가 나 자신이라면
+        if(photonView.IsMine)
+        {
+            Camera.main.gameObject.SetActive(false);
+        }
+        else
+        {
+            temporaryCamera.enabled = false;
+            GetComponentInChildren<AudioListener>().enabled = false;
+        }
+    }
+
     void Update()
     {
-        
+        if(!photonView.IsMine) return;
+
+        MoveMent(Time.deltaTime);
+
+        mouseX += Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+
+        transform.eulerAngles = new Vector3(0, mouseX, 0);
+
     }
+
+
+
+    public void MoveMent(float time)
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+
+        direction.Normalize();
+
+        transform.position += transform.TransformDirection(direction) * speed * time;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        // 로컬 오브젝트라면 쓰기 부분을 실행합니다.
+        if(stream.IsWriting)
+        {
+            stream.SendNext(score);
+        }
+        else // 원격 오브젝트라고 하면 읽기 부분을 실행합니다.
+        {
+            // 네트워크를 통해서 데이터를 받습니다.
+            score = (float)stream.ReceiveNext();
+        }
+
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Preasure Box"))
+        {
+            PhotonView view = other.GetComponent<PhotonView>();
+
+            if(view.IsMine)
+            {
+                score++;
+
+                PhotonNetwork.Destroy(view);
+
+            }
+
+
+        }
+
+    }
+
+
 }
